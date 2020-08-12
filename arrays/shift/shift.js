707f4e5ca4b18f05e84294cbf86d0d4bf11d5e33
BrendonLH <Brendonhampton1988@gmail.com>
2020-08-11 18:48:57 -0700

    added the code, adding the jpg from githubas I cant migrate somehow

--- arrays/shift/shift.js
+++ arrays/shift/shift.js
@@ -1,8 +1,29 @@
 'use strict';
 
+let arr = [2,4,6,8];
+let num = 5;
 
-//  push method
-// let push = ((arr, val) => {
-//     array[arr.length] = val;
-// })
+let insertShiftArray = (arr, num) => {
+  let midArr = Math.ceil((arr.length-1)/2);
+  let newArr = [];
+  let j = 0; 
+  for(let i = 0; i <= arr.length; i++) { 
+    if( i == midArr) {
+      newArr[i] = num;
+    }
+    else {
+      newArr[i] = arr[j];
+      j++;
+    }
+    
+    
+  }
+  return newArr;
+}
+
+console.log(insertShiftArray(arr, num));
+
+
+
+
 
