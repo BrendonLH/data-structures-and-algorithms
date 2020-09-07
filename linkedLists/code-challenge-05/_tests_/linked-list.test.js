5b4d03c1df40bb2a694453d37061565aae520bcb
BrendonLH <Brendonhampton1988@gmail.com>
2020-09-06 20:12:10 -0700

    added passing tests and added feature that partially works

--- linkedLists/code-challenge-05/_tests_/linked-list.test.js
+++ linkedLists/code-challenge-05/_tests_/linked-list.test.js
@@ -1,1 +1,13 @@
 'use strict';
+
+let list = require('../linked-list');
+
+it('checks if head node value was what was added', () => {
+    list.add('test1');
+    console.log(list);
+    expect(list.head.value).toEqual('test1');
+});
+
+it('checks if something was added to the list', () => {
+    expect(list.head).toBeTruthy();
+})
